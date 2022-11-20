2e2a554e0113073d0a9913b938f79da5ef21d77b
Fedor Garanin <yesfedor.go@gmail.com>
2022-11-20 14:12:45 +0400

    Save

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Profile
+  </div>
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router'
+import { useAuth } from '../plugins/Auth'
+
+export default {
+  name: 'UserProfile',
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+    const Api = useAuth()
+    Api.install(1, {
+      routerPush: routeName => {
+        console.log('routeName: ', routeName)
+        if (routeName === 'Current') router.push({ name: route.name })
+      },
+      storeCommit: (what, payload) => {
+        console.log('storeCommit: ', what, payload)
+      }
+    })
+    Api.mounted()
+    console.log(Api)
+  }
+}
+</script>
